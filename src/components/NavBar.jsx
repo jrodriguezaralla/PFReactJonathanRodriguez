f17f9875a98f502ba17bf1adf4aca15dca8762a9
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-02-03 00:16:55 -0300

    incluyendo bootstrap en el proyecto y customizando estilos de NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 const NavBar = () => {
     return(
-        <div>NavBar</div>
+        <>
+            <Navbar bg="light">
+                <Container>
+                    <Navbar.Brand href="#home">
+                    <img
+                        src="./src/assets/logo.svg"
+                        width="80"
+                        height="80"
+                        className="d-inline-block align-top"
+                        alt="Enersys Baterias logo"
+                    />
+                    </Navbar.Brand>
+                    <NavDropdown title="Categorias" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#">Categoria 1</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Categoria 2</NavDropdown.Item>
+                    </NavDropdown>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
