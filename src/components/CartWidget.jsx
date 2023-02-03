639f5fd10767cc6fdb631ca1d2ff98d2b6eb63ce
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-02-03 15:59:41 -0300

    agregando un fonde al body

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,7 +3,7 @@ import Badge from 'react-bootstrap/Badge';
 const CartWidget = () => {
     return(
         <div className="position-relative">
-            <span className="material-symbols-outlined Cart__Size">
+            <span className="material-symbols-outlined Cart--Size">
                 shopping_cart
             </span>
             <Badge pill bg="danger" className="position-absolute top-50 start-50 ms-3 translate-middle">
