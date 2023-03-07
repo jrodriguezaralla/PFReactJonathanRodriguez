851dc496e7adf3a6a4df26d55a69456bdced23b5
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-03-06 22:21:45 -0300

    accediendo a collecion y document en firebase

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { useState, useEffect } from "react"
 import ItemDetail from './ItemDetail'
 import { useParams } from 'react-router-dom'
-
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
 const ItemDetailContainer = () => {
     const {id} = useParams()
 
-    const consultarProductos = async () => {
-        const respuesta = await fetch ("/datos.json")
-        const resultado = await respuesta.json()
-
-        return resultado
-    }
-
     const [productDetail, setProductDetail] = useState([])
     
     useEffect(() => {
-        consultarProductos().then((productDetail) => setProductDetail(productDetail.find ((prod) => prod.id == id)))
+        const db = getFirestore()
+
+        const batRef= doc(db, "baterias", `${id}`)
+        getDoc(batRef).then((snapshot) => {
+            if(snapshot.exists()){
+                setProductDetail({id: snapshot.id, ...snapshot.data()})
+            }
+        })
+        
     }, []);
 
     return (
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,22 +2,24 @@ import ItemList from "./ItemList"
 import { useEffect, useContext } from "react"
 import { useParams } from 'react-router-dom'
 import { CartContext } from "../context/ShoppingCartContext"
+import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 
 const ItemListContainer = () => {
     const {category} = useParams()
     const {productos, setProductos} = useContext(CartContext)
-
-    const consultarProductos = async () => {
-        const respuesta = await fetch ("/datos.json")
-        const resultado = await respuesta.json()
-
-        return resultado
-    }
-    
     
     useEffect(() => {
-        consultarProductos().then((productos) => setProductos(productos))
+        const db = getFirestore();
+        const itemsCollection = collection(db, "baterias");
+        getDocs(itemsCollection).then((snapshot) => {
+            const productos = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+                
+            }));
+            setProductos(productos);
+        });
     }, []);
     
     const prodFilter = productos.filter ((prod) => prod.category === category)
