3904d290b93122825e9969f7e536729951e92d50
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-03-13 16:33:43 -0300

    guardando orden en base de datos con info de cliente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../index.css';
+import { collection, getFirestore, addDoc} from 'firebase/firestore';
+import { CartContext } from '../context/ShoppingCartContext';
 
 const Formulario = () => {
+    const {cart} = useContext (CartContext)
+    const [orderId, setOrderId] = useState (null)
+    const [name, setName] = useState("")
+    const [email,setEmail] = useState("")
+
+    const order = {
+        name,
+        email,
+        cart
+    }
+    console.log(name)
+    const db = getFirestore()
+    const ordersCollection = collection(db,"orden")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addDoc(ordersCollection, order).then(({id})=> setOrderId(id))
+    }
+
     return (
         <div className='d-flex flex-row justify-content-center my-5 fade-in'>
             <div border="light" className="form--container w-50"> 
                 <h1 className='fs-3 m-0 fw-bold text-dark text-center'>INGRESE SUS DATOS DE CONTACTO</h1>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                         <Form.Label >Nombre:</Form.Label>
-                        <Form.Control type="Text" placeholder="Ingrese su nombre" className="mb-3" required/>
+                        <Form.Control type="Text" placeholder="Ingrese su nombre" className="mb-3" required onChange={(e) => setName(e.target.value)}/>
                         <Form.Label>Apellido:</Form.Label>
                         <Form.Control type="Text" placeholder="Ingrese su apellido" className="mb-3" required/>
                         <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" className="mb-3" required/>
+                        <Form.Control type="email" placeholder="name@example.com" className="mb-3" required onChange={(e) => setEmail(e.target.value)}/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Dejenos un mensaje:</Form.Label>
@@ -25,6 +46,7 @@ const Formulario = () => {
                         Finalizar compra
                     </Button>
                 </Form>
+                <h2>Nro de orden: {orderId}</h2>
             </div>
         </div>
     )
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -3,13 +3,16 @@ import { useEffect, useContext } from "react"
 import { useParams } from 'react-router-dom'
 import { CartContext } from "../context/ShoppingCartContext"
 import { collection, getDocs, getFirestore } from "firebase/firestore";
+import Loader from "./Loader";
 
 
 const ItemListContainer = () => {
     const {category} = useParams()
-    const {productos, setProductos, setLoading, setCount} = useContext(CartContext)
+    const {productos, setProductos, setLoading, setCount, loading} = useContext(CartContext)
     
+
     useEffect(() => {
+        setLoading(true)
         const db = getFirestore();
         const itemsCollection = collection(db, "baterias");
         getDocs(itemsCollection).then((snapshot) => {
@@ -21,11 +24,17 @@ const ItemListContainer = () => {
             setProductos(productos);
             setCount(1);
         })
-        setLoading(false)
+        setTimeout(()=>{
+            setLoading(false)
+        },100)
         
     }, [])
-    setLoading(true)
+    
     const prodFilter = productos.filter ((prod) => prod.category === category)
+    if(loading)
+    {
+        return <Loader/>
+    }
 
     return (
         <div className="w-100 h-100">
