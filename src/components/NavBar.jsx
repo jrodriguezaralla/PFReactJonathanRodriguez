1aa3d79fba26a4a0235900949b6eeae54375df72
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-05-02 19:05:24 -0300

    modificando path de imagen logo para que se vea en github page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,52 +3,40 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
-import '../index.css'
+import '../index.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-    return(
-        <>
-            <Navbar bg="light">
-                <Container className='navBar--height'>
-                    <Link to="/">
-                        <Navbar.Brand>
-                            <img
-                            src="/logo.svg"
-                            width="120"
-                            height="120"
-                            className="d-inline-block align-top"
-                            alt="Enersys Baterias logo"
-                            />
-                        </Navbar.Brand>
-                    </Link>
+	return (
+		<>
+			<Navbar bg="light">
+				<Container className="navBar--height">
+					<Link to="/">
+						<Navbar.Brand>
+							<img src="logo.svg" width="120" height="120" className="d-inline-block align-top" alt="Enersys Baterias logo" />
+						</Navbar.Brand>
+					</Link>
 
-                    <DropdownButton
-                        id="dropdown-button-dark-example2"
-                        variant="dark"
-                        menuVariant="dark"
-                        title="Categorias"
-                        className="mt-2"
-                    > 
-                        <Dropdown.Item as='div'>
-                            <Link to={`/category/${"PLOMO-CALCIO"}`} className="text-decoration-none text-white">
-                                PLOMO-CALCIO
-                            </Link>
-                        </Dropdown.Item>
-                        <Dropdown.Item as='div'>
-                            <Link to={`/category/${"PLOMO-PURO"}`} className="text-decoration-none text-white">
-                                PLOMO PURO
-                            </Link>
-                        </Dropdown.Item>   
-                    </DropdownButton>
+					<DropdownButton id="dropdown-button-dark-example2" variant="dark" menuVariant="dark" title="Categorias" className="mt-2">
+						<Dropdown.Item as="div">
+							<Link to={`/category/${'PLOMO-CALCIO'}`} className="text-decoration-none text-white">
+								PLOMO-CALCIO
+							</Link>
+						</Dropdown.Item>
+						<Dropdown.Item as="div">
+							<Link to={`/category/${'PLOMO-PURO'}`} className="text-decoration-none text-white">
+								PLOMO PURO
+							</Link>
+						</Dropdown.Item>
+					</DropdownButton>
 
-                    <Link to={"/cart"} className="text-decoration-none text-dark">
-                        <CartWidget/>
-                    </Link>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
+					<Link to={'/cart'} className="text-decoration-none text-dark">
+						<CartWidget />
+					</Link>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
 
-export default NavBar
+export default NavBar;
